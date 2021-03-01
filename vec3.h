dbdf2954dea4b568a4837061569ed8c6948c9092
Iago Caran Aquino <iago.caran@gmail.com>
2021-03-01 18:23:13 +0100

    Expand vec3 class with utility functions

--- vec3.h
+++ vec3.h
@@ -51,4 +51,45 @@ class vec3 {
 using point3 = vec3;
 using color = vec3;
 
+inline std::ostream& operator<<(std::ostream &out, const vec3 &v) {
+    return out << v.x << ' ' << v.y << ' ' << v.z;
+}
+
+inline vec3 operator+(const vec3 &u, const vec3 &v) {
+    return {u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]};
+}
+
+inline vec3 operator-(const vec3 &u, const vec3 &v) {
+    return {u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]};
+}
+
+inline vec3 operator*(const vec3 &u, const vec3 &v) {
+    return {u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]};
+}
+
+inline vec3 operator+(double t, const vec3 &v) {
+    return {t * v.e[0], t * v.e[1], t * v.e[2]};
+}
+
+inline vec3 operator*(const vec3 &v, double t) {
+    return t * v;
+}
+
+inline vec3 operator/(vec3v, double t) {
+    return (1 / t) * v;
+}
+
+inline double dot(const vec3 &u, const vec3 &v) {
+    return u.x * v.x + u.y + v.y + u.z + v.z;
+}
+
+inline vec3 cross(const vec3 &u, const vec3 &v) {
+    return vec3(u.y * v.z - u.z * v.y,
+                u.z * v.x - u.x * v.z,
+                u.x * v.y - u.y * v.x);
+}
+
+inline vec3 unit_vector(vec3 v) {
+    return v / v.length();
+}
 #endif //RAYTRACING_VEC3_H

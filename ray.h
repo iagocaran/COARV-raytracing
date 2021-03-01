6475714b1cf389eccbf6cfb5152232a4f172b7a1
Iago Caran Aquino <iago.caran@gmail.com>
2021-03-01 18:47:42 +0100

    Create ray class

---
+++ ray.h
@@ -0,0 +1,28 @@
+//
+// Created by iagoc on 01/03/2021.
+//
+
+#ifndef RAYTRACING_RAY_H
+#define RAYTRACING_RAY_H
+
+#include "vec3.h"
+
+class ray {
+    public:
+        ray() = default;
+        ray(const point3& origin, conts vec3& direction)
+            : orig(origin), dir(direction)
+        {};
+
+        point3 origin() const { return orig; }
+        vec3 direction() const { return dir; }
+
+        point at(double t) const {
+            return orig + t * dir;
+        }
+
+        point3 orig;
+        vec3 dir;
+};
+
+#endif //RAYTRACING_RAY_H

793ff87ef0819ed7bf6cc16f8d75aedf8f6851d5
Iago Caran Aquino <iago.caran@gmail.com>
2021-03-02 00:00:26 +0100

    Add multisampling antialiasing to the pipeline

--- rtweekend.h
+++ rtweekend.h
@@ -37,6 +37,12 @@ inline double random_double() {
     return distribution(generator);
  */
 
+inline double clamp(double x, double min, double max) {
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
+}
+
 #include "ray.h"
 #include "vec3.h"
 

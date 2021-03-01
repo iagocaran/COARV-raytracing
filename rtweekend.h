3a372ae01c3d7fe17f80fad3c20390578f1d6c30
Iago Caran Aquino <iago.caran@gmail.com>
2021-03-01 22:45:46 +0100

    Expand rtweekend with random generators

--- rtweekend.h
+++ rtweekend.h
@@ -8,6 +8,8 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <cstdlib>
+// #include <random>
 
 using std::shared_ptr;
 using std::make_shared;
@@ -19,6 +21,21 @@ const double pi = 3.1415926535897932385;
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180;
 }
+
+inline double random_double() {
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    return min + (max - min) * random_double();
+}
+
+/* Commenting the C++ code as rand() is actually faster and this application doesn't need a lot of security
+inline double random_double() {
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+ */
 
 #include "ray.h"
 #include "vec3.h"

198f9c7265868eb9104e958868bc1be4c97b58b5
Iago Caran Aquino <iago.caran@gmail.com>
2021-03-01 20:46:32 +0100

    Create the hittable class

---
+++ hittable.h
@@ -0,0 +1,22 @@
+//
+// Created by iagoc on 01/03/2021.
+//
+
+#ifndef RAYTRACING_HITTABLE_H
+#define RAYTRACING_HITTABLE_H
+
+#include "ray.h"
+
+struct hit_record {
+    point3 p;
+    vec3 normal;
+    double t;
+};
+
+class hittable {
+    public:
+        virtual bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const = 0;
+};
+
+
+#endif //RAYTRACING_HITTABLE_H

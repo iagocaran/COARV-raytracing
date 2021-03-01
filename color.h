768bf61248d87a9ed546b42c676ae8809d56730d
Iago Caran Aquino <iago.caran@gmail.com>
2021-03-01 18:28:07 +0100

    Add color type utility header

---
+++ color.h
@@ -0,0 +1,18 @@
+//
+// Created by iagoc on 01/03/2021.
+//
+
+#ifndef RAYTRACING_COLOR_H
+#define RAYTRACING_COLOR_H
+
+#include "vec3.h"
+
+#include <iostream>
+
+void write_color(std::ostream &out, color pixel_color) {
+    out << static_cast<int>(255.999 * pixel_color.x()) << ' '
+        << static_cast<int>(255.999 * pixel_color.y()) << ' '
+        << static_cast<int>(255.999 * pixel_color.z()) << '\n';
+}
+
+#endif //RAYTRACING_COLOR_H

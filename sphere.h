5cfb3427c42c726ff1f8dbe21a0c11814ba77f4e
Iago Caran Aquino <iago.caran@gmail.com>
2021-03-02 12:02:08 +0100

    Fix clang warnings

--- sphere.h
+++ sphere.h
@@ -10,7 +10,7 @@
 
 class sphere : public hittable {
     public:
-        sphere() {}
+        sphere() = default;
         sphere(point3 cen, double r, shared_ptr<material> m) : center(cen), radius(r), mat_ptr(m) {};
 
         bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const override;

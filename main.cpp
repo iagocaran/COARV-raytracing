deab70b5a36de12da782638315c2250b7963c70a
Iago Caran Aquino <iago.caran@gmail.com>
2021-03-03 21:22:14 +0100

    Fix missing variable for getting sphere position

--- main.cpp
+++ main.cpp
@@ -36,7 +36,7 @@ hittable_list random_scene() {
     for (int a = -11; a < 11; a++) {
         for (int b = -11; b < 11; b++) {
             auto choose_mat = random_double();
-            point3 center(a + 0.9 * random_double(), 0.2, 0.9 * random_double());
+            point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
 
             if ((center - point3(4, 0.2, 0)).length() > 0.9) {
                 shared_ptr<material> sphere_material;
